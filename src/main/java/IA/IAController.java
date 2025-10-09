4f899c99f7ce163004913c22e185ed3523ac7dad
YusufT7 <159006515+YusufT7@users.noreply.github.com>
2025-10-09 08:10:54 +0300

    october 7

--- src/main/java/IA/IAController.java
+++ src/main/java/IA/IAController.java
@@ -4,8 +4,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -51,53 +53,70 @@ public class IAController {
     CheckBox chkPlate = new CheckBox();
     @FXML
     Label labelNum = new Label();
+    @FXML
+    Label labelPhoto = new Label();
+    @FXML
+    Label labelRegistration = new Label();
+    @FXML
+    Label labelRegPhoto = new Label();
     Connection conn;
 
     public IAController() throws SQLException{
         conn = MyConnection.connectDB();
     }
 
+    public void initializeStickerNum(){
+        // add code here where the label for sticker number is randomly generated
+        // make sure the sticker number is stored in database(?)
+    }
+    public void initialize(){
+        //this is done so that the label shows multiple lines instead of keeping it as one
+        labelRegistration.setText("(Any documents proof the ownership of the " +
+                "\n motorized vehicle (e.g. government " +
+                "\n registration, purchase order, sale " +
+                "\n invoices, email agreement, etc...))");
+    }
 
     public void onClickRegister(ActionEvent actionEvent) throws SQLException, IOException {
         //gets all input values and assigns them to variables
-//        String first_name = txtFName.getText();
-//        String last_name = txtLName.getText();
-//        int userId = Integer.parseInt(txtID.getText());
-//        String phone_num = txtMobile.getText();
-//        String serialNum = txtSerialNum.getText();
-//        String vehicleType = (String) cmbType.getValue();
-//        String brand = (String) cmbBrand.getValue();
-//        String primColor = String.valueOf(cmbPrimColor.getValue());
-//        String secColor = String.valueOf(cmbSecColor.getValue());
-//
-//        //adds info to database
-//        //users
-//        String query1 = "SELECT * FROM users WHERE first_name=? AND last_name=? AND phone_num=? AND user_id=?";
-//        PreparedStatement ps = conn.prepareStatement(query1);
-//        ps.setString(1, first_name);
-//        ps.setString(2, last_name);
-//        ps.setString(3, phone_num);
-//        ps.setString(4, String.valueOf(userId));
-//
-//        //vehicles
-//        String query2 = "SELECT * FROM vehicles WHERE 1 primColor=? AND vehicleType=? AND serialNum=? AND status=? AND brand=? AND secColor=?";
-//        ResultSet rs = ps.executeQuery();
-//
-//        if (rs.next()){
-//            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-//            alert.setTitle("welcome");
-//            alert.setHeaderText(null);
-//            alert.setContentText("Login successful");
-//            alert.showAndWait();
-//        }
-//        else{
-//            Alert alert = new Alert(Alert.AlertType.ERROR);
-//            txtFName.setText("");
-//            txtLName.setText("");
-//            txtMobile.setText("");
-//            txtSerialNum.setText("");
-//            cmbType.getItems().clear();
-//        }
+        String first_name = txtFName.getText();
+        String last_name = txtLName.getText();
+        int userId = Integer.parseInt(txtID.getText());
+        String phone_num = txtMobile.getText();
+        String serialNum = txtSerialNum.getText();
+        String vehicleType = (String) cmbType.getValue();
+        String brand = (String) cmbBrand.getValue();
+        String primColor = String.valueOf(cmbPrimColor.getValue());
+        String secColor = String.valueOf(cmbSecColor.getValue());
+
+        //adds info to database
+        //users
+        String query1 = "SELECT * FROM users WHERE first_name=? AND last_name=? AND phone_num=? AND user_id=?";
+        PreparedStatement ps = conn.prepareStatement(query1);
+        ps.setString(1, first_name);
+        ps.setString(2, last_name);
+        ps.setString(3, phone_num);
+        ps.setString(4, String.valueOf(userId));
+
+        //vehicles
+        String query2 = "SELECT * FROM vehicles WHERE 1 primColor=? AND vehicleType=? AND serialNum=? AND status=? AND brand=? AND secColor=?";
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("welcome");
+            alert.setHeaderText(null);
+            alert.setContentText("Login successful");
+            alert.showAndWait();
+        }
+        else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            txtFName.setText("");
+            txtLName.setText("");
+            txtMobile.setText("");
+            txtSerialNum.setText("");
+            cmbType.getItems().clear();
+        }
 
         //code here to transfer to next fxml ui (WebsiteHome.fxml)
         FXMLLoader fxmlLoader2 = new FXMLLoader(IAApplication.class.getResource("WebsiteHome.fxml"));
@@ -108,13 +127,27 @@ public class IAController {
     }
 
     // code here for choosing photo in IA.fxml (when registering vehicle)
-//    public void onClickPhoto(ActionEvent actionEvent) throws SQLException{
-//
-//        FileChooser fileChooser = new FileChooser();
-//        fileChooser.setTitle("Select Photo File");
-//        File file = fileChooser.showOpenDialog(null);
-//        if (file != null){
-//            //code here
-//            }
+    // source: stackoverflow
+    public void onClickVehiclePhoto(ActionEvent actionEvent) throws SQLException {
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Photo File");
+        File photo = fileChooser.showOpenDialog(null);
+        if (photo != null) {
+            //code here
+            labelPhoto.setText("Selected photo: " + photo.getName());
+        }
+    }
+
+    //same as onClickVehiclePhoto()
+    public void onClickRegistrationPhoto(ActionEvent actionEvent) throws SQLException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Photo File");
+        File photo = fileChooser.showOpenDialog(null);
+        if (photo != null) {
+            labelRegPhoto.setText("Selected photo: " + photo.getName());
+        }
+    }
+
 }
 
